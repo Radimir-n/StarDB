9d05bef44b19d8df6957be817976ef2ee4efefdc
au_radimir <radimir@theforest.ru>
2021-02-19 17:16:02 +0500

    Начало работы с content

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -5,18 +5,52 @@ export const ComponentContent = ({activePage}) => {
 
   const state = useSelector(state => state)
   const dispatch = useDispatch()
+  const [currentContent, setCurrentContent] = useState([])
 
   useEffect(() => {
 
-  }, [])
+    
+    let czList = document.getElementsByClassName('contentZone')
+
+    // cz.style.zIndex = 4;
+    // console.log(cz)
+    // if(cz.length != 0){
+    //   for(let element in cz){
+    //     console.log(element)
+    //   }
+    // }
+  }, [currentContent])
+  
+  //   cz.onclick = function() {
+  //     console.log(cz)
+  //   }
+
+    // document.onclick = function() {
+    //   if (leftMenuOpened && !clickWasInLeftMenu) {
+    //     toggleLeftMenu(false)
+    //   }
+    //   clickWasInLeftMenu = false
+    // }
+
 
   function onCreateContent(){
     let contentArray = [
-      1,2
+      1
     ]
     let content = contentArray.map(item => {
+      let zIndex = 2
+      let setStyle = ''
+      if(currentContent > 0){
+        if(currentContent == item){
+          zIndex = 4 
+          setStyle = "activeContent"
+        }
+        else{
+          zIndex = 0
+        }
+      }
       return (
-        <div className='contentZone' key={item}>{item}
+        <div id = {`zone +${item}`} className={`contentZone ${setStyle}`} key={item} onClick={() => {setCurrentContent(item)}}style = {{zIndex: zIndex}}>{item}
           <ul>
             <li>name</li>
             <li>gender</li>
@@ -29,6 +63,7 @@ export const ComponentContent = ({activePage}) => {
     })
     return content
   }
+  
   let contentZone = onCreateContent()
   if(activePage == "Planets"){
     return (
@@ -39,8 +74,11 @@ export const ComponentContent = ({activePage}) => {
   }
   else if(activePage == "Person"){
     return (
-      <div className='content'>
-          {contentZone}
+      <div>
+        <div className='content'>
+            {contentZone}
+        </div>
+        <div className="transBox"></div>
       </div>
     )
   }
