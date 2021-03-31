4507fd2e45527be6e9e92b8c02e7ede723cab0a1
au_radimir <radimir@theforest.ru>
2021-03-31 17:17:27 +0500

    1

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -16,44 +16,79 @@ export const ComponentContent = ({activePage, onActivePage}) => {
   const [personId, setPersonId] = useState([])
   const [loading, setLodingIndicator] = useState(true)
   const [page, getActivePageData] = useState([])
+  const [content, setActiveContent] = useState(null)
 
 
   useEffect(() => {
-    getData()
+    services.getAllPlanets()
+    .then(data=>{
+      setPlanetsData(data)
+      setLodingIndicator(false)
+    })
+    services.getAllPeople()
+    .then(data=>{
+      setPeopleData(data)
+      setLodingIndicator(false)
+    })
+    services.getAllStarships()
+    .then(data=>{
+      setStarshipsData(data)
+      setLodingIndicator(false)
+    })
+    services.getFilms()
+    .then(data=>{
+      setFilmsData(data)
+      setLodingIndicator(false)
+    })
   }, [])
-  useEffect(() => {
-    // return () => {
-    //   getPerson()
-    // };
-  }, []);
 
-    async function getData(){
-
-      let allPlanets = await services.getAllPlanets()
-      setPlanetsData(allPlanets)
-      setLodingIndicator(false)
-
-      let allPeople = await services.getAllPeople()
-      setPeopleData(allPeople)
-      setLodingIndicator(false)
-
-      let allStarships = await services.getAllStarships()
-      setStarshipsData(allStarships)
-      setLodingIndicator(false)
-
-      let allFilms = await services.getFilms()
-      setFilmsData(allFilms)
-      setLodingIndicator(false)
+  useEffect(()=>{
+    onCreateContent(peopleData, activePage)
+    .then((data)=>{
+      setActiveContent(data)
+    })
+  },[peopleData])
+  // return (
+  //   <div className='content'>
+  //     <Spinner
+  //       loading = {loading}
+  //     />
+  //       {content}
+  //   </div>
+  // )
+  useEffect(()=>{
+    let activeData = undefined
+    if(activePage != "Person"){
+      if(activePage == "planets"){
+        activeData = planetsData
+      }
+      else if(activePage == "characters"){
+        activeData = peopleData
+      }
+      else if(activePage == "starships"){
+        activeData = starshipsData
+      }
+      else if(activePage == "films"){
+        activeData = filmsData
+      }
+      onCreateContent(activeData, activePage)
+      .then((data)=>{
+        setActiveContent(data)
+      })
+    }else{
+      console.log(page, personId)
+      onCreatePersonData(page, personId)
     }
+  },[activePage])
 
-  function onCreateContent(data, img){
+   async function onCreateContent(data, img){
     let index = 0
     let personData = [
       data,
       img
     ]
-    
-    let content = data.map(item => {
+    let arrResult = []
+    for(let item of data){
       let id = services._extractId(item.url)
       let name = undefined
       if(!item.name){
@@ -64,24 +99,30 @@ export const ComponentContent = ({activePage, onActivePage}) => {
       }
       index = index + 1
       if(loading){
-        return (
-            <div  className= "contentZone" key = {index} onClick={() => {setCurrentContent(index -1)}}>
+        const newItem = <div  className= "contentZone" key = {index} onClick={() => {setCurrentContent(index -1)}}>
                 <Spinner/>
             </div>
-        )
+        arrResult = [...arrResult, newItem]
       }
       else{
-        return (
-            <div className= "contentZone" key = {index} onClick={() => {setCurrentContent(index - 1); setPersonId(id); onActivePage("Person"); getActivePageData(personData)}}>
+        let response = await fetch('https://starwars-visualguide.com/assets/img/'+ img+'/'+ id + '.jpg')
+        let result = `https://starwars-visualguide.com/assets/img/${img}/${id}.jpg`
+        if(response.status == 404){
+          result = 'https://starwars-visualguide.com/assets/img/big-placeholder.jpg'
+        }
+        
+        const newItem = <div className= "contentZone" key = {index} onClick={() => {setCurrentContent(index - 1); setPersonId(id); onActivePage("Person"); getActivePageData(personData)}}>
               <div className="image">
-                <img src={`https://starwars-visualguide.com/assets/img/${img}/${id}.jpg`}className = "image_center"/>
+                <img src={result}className = "image_center"/>
               </div>
               <h2 className = "person_name">{name}</h2>
            </div>
-        )
+        arrResult = [...arrResult, newItem]
+            
+
       }
-    })
-    return content
+    }
+    return arrResult
   }
 
   function onCreatePersonData(data, id){
@@ -117,13 +158,13 @@ export const ComponentContent = ({activePage, onActivePage}) => {
       let planet_id =  services._extractId(homeworld.url)
       list = [
         <div key = 'people'>
-            <li>gender: {person_data.gender}</li>
-            <li>birthTear: {person_data.birth_year}</li>
-            <li>hair: {person_data.hair_color}</li>
-            <li>eye color: {person_data.eye_color}</li>
-            <li>mass: {person_data.mass}</li>
-            <li>height: {person_data.height}</li>
-            <li>homeworld: <a href="#" onClick={() => {onCreatePersonData(planet_link,planet_id),setPersonId(planet_id),getActivePageData(planet_link)}}>{homeworld.name}</a> </li>
+            <li>Gender: {person_data.gender}</li>
+            <li>BirthTear: {person_data.birth_year}</li>
+            <li>Hair: {person_data.hair_color}</li>
+            <li>Eye color: {person_data.eye_color}</li>
+            <li>Mass: {person_data.mass}</li>
+            <li>Height: {person_data.height}</li>
+            <li>Homeworld: <a href="#" onClick={() => {onCreatePersonData(planet_link,planet_id),setPersonId(planet_id),getActivePageData(planet_link)}}>{homeworld.name}</a> </li>
         </div>
       ]
     }
@@ -138,13 +179,13 @@ export const ComponentContent = ({activePage, onActivePage}) => {
       ]
       list = [
         <div key = 'planets'>
-              <li>population: {person_data.population}</li>
-              <li>diameter: {person_data.diameter} km</li>
-              <li>gravity: {person_data.gravity}</li>
-              <li>orbital period: {person_data.orbital_period} days</li>
-              <li>climate: {person_data.climate}</li>
-              <li>surface water: {person_data.surface_water}%</li>
-              <li>rotation period: {person_data.rotation_period} days</li>
+              <li>Population: {person_data.population}</li>
+              <li>Diameter: {person_data.diameter} km</li>
+              <li>Gravity: {person_data.gravity}</li>
+              <li>Orbital period: {person_data.orbital_period} days</li>
+              <li>Climate: {person_data.climate}</li>
+              <li>Surface water: {person_data.surface_water}%</li>
+              <li>Rotation period: {person_data.rotation_period} days</li>
         </div>
       ]
     }
@@ -233,7 +274,9 @@ export const ComponentContent = ({activePage, onActivePage}) => {
           <div key = {id} className = 'object_image'>
             <ul>
               <img src={`https://starwars-visualguide.com/assets/img/${page}/${id}.jpg`} className = "image_object"/>
+              <div className = 'link_name'>
               <a href="#" onClick={() => {onCreatePersonData(objects_data,object_id),setPersonId(object_id),getActivePageData(objects_data)}}>{name}</a>
+              </div>
             </ul>
           </div>
           
@@ -254,49 +297,12 @@ export const ComponentContent = ({activePage, onActivePage}) => {
     ]
     return list
   }
-  let content = undefined
-  if(activePage == "planets"){
-    content = onCreateContent(planetsData, activePage)
-    return (
-      <div className='content'>
-          {content}
-      </div>
-    )
-  }
-  else if(activePage == "characters"){
-    content = onCreateContent(peopleData, activePage)
-    return (
-      <div className='content'>
-        <Spinner
-          loading = {loading}
-        />
-          {content}
-      </div>
-    )
-  }
-  else if(activePage == "starships"){
-    content = onCreateContent(starshipsData, activePage)
-    return (
-      <div className='content'>
-          {content}
-      </div>
-    )
-  }
-  else if(activePage == "Person"){
-    content = onCreatePersonData(page, personId)
-    return (
-      <div className='personContent'>
-          {content}
-      </div>
-    )
-  }
-  else if(activePage == "films"){
-    content = onCreateContent(filmsData, activePage)
-    return (
-      <div className='personContent'>
-          {content}
-      </div>
-    )
-  }
+  // let content = undefined
+  return(
+    <div className='content'>
+        {content}
+    </div>
+  )
+
 
 }
