5b7b8044d016b9da5330dcbb7371a69aece0a836
au_radimir <radimir@theforest.ru>
2021-04-01 10:20:15 +0500

    Чистим лишние функции

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -12,26 +12,14 @@
     const res =  await getResource (`/people/`); 
     return res.results
   } 
-  export async function getPerson(id) { 
-    const person =  await getResource (`/people/${id}/`); 
-    return _transformPerson(person)
-  } 
   export async function getAllPlanets(){  
     const res = await getResource (`/planets/`); 
     return res.results
-  } 
-  export async function getPlanet(id){
-    const planet = await getResource (`/planets/${id}/`);
-    return _transformPlanet(planet) 
   } 
   export async function getAllStarships () { 
     const res = await getResource (`/starships/`);
     return res.results
   } 
-  export async function getStarship (id){ 
-    const starships = getResource (`/starships/${id}/`);
-    return  _transformStarship(starships)
-  }
   export async function getFilms(){
     const res =  await getResource (`/films/`); 
     return res.results
